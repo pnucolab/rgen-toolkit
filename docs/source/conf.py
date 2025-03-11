9a4cd422ffa006f534d09571db512c8795b92b7f
Abyot <89458721+abyot2021@users.noreply.github.com>
2025-03-11 11:23:47 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'VarCas-OFFinder'
+copyright = '2024, COLAB@PNU'
+author = 'Abyot Melkamu'
 
 release = '0.1'
 version = '0.1.0'
